36181f89cb2ef2e516d8df34793bb13e7c8e6733
uuiikk <schabanovyulian@gmail.com>
2024-06-16 17:11:41 +0300

    Creating markup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WeatherApp from "./components/WeatherApp"
+import "./components/WeatherApp.css"
+const App = () => {
+  return (
+    <div>
+        <WeatherApp />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,43 @@
+import sunny from "../assets/images/sunny.png"
+
+
+const WeatherApp = () => {
+  return (
+		<div className='container'>
+			<div className='weather-app'>
+				<div className='search'>
+					<div className='search-top'>
+						<i className='fa-solid fa-location-dot'></i>
+						<div className='location'>London</div>
+					</div>
+					<div className='search-bar'>
+						<input type='text' placeholder='Введите местоположение' />
+						<i className='fa-solid fa-magnifying-glass'></i>
+					</div>
+				</div>
+				<div className='weather'>
+					<img src={sunny} alt='sunny' />
+					<div className='weather-type'>Clear</div>
+					<div className='temp'>28°C</div>
+				</div>
+				<div className='weather-date'>
+					<p>Воскресенье, 16.06</p>
+				</div>
+				<div className='weather-data'>
+					<div className='humidity'>
+						<div className='data-name'>Влажность</div>
+						<i className='fa-solid fa-droplet'></i>
+						<div className='data'>35%</div>
+					</div>
+					<div className='wind'>
+						<div className='data-name'>Ветер</div>
+						<i className='fa-solid fa-wind'></i>
+						<div className='data'>3 км/ч</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default WeatherApp
