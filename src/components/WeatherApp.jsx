0c38401790335d84dcb812d55862ce9dc3e4ec30
uuiikk <schabanovyulian@gmail.com>
2024-06-19 02:57:26 +0300

    Add animated styles v0.2.1 and delete search button

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,19 +1,27 @@
-import sunny from "../assets/images/sunny.png"
-
+import sunny from '../assets/images/sunny.png'
 
 const WeatherApp = () => {
-  return (
+	return (
 		<div className='container'>
 			<div className='weather-app'>
 				<div className='search'>
 					<div className='search-top'>
-						<i className='fa-solid fa-location-dot'></i>
-						<div className='location'>Лондон</div>
+						<div className='location'>
+							<i className='fa-solid fa-location-dot'></i>
+							<label className='city'>Санкт-Петербург</label>
+						</div>
 					</div>
 					<div className='search-bar'>
 						<input type='text' required />
 						<label htmlFor=''>Введите местоположение</label>
-						<i className='fa-solid fa-magnifying-glass'></i>
+						<div className='clear-button'>
+							<i className='fa-solid fa-xmark'></i>
+							<button className='clear-btn' />
+						</div>
+						<div className='search-button'>
+							<i className='fa-solid fa-search'></i>
+							<button className='search-btn' />
+						</div>
 					</div>
 				</div>
 				<div className='weather'>
